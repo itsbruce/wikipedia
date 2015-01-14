3873f23c63e40381b1bac78d41452ac07c8e95af
Bruce Richardson <it.is.bruce@gmail.com>
2015-01-14 13:48:58 +0000

    "Duck typing" in Scala with structural types

---
+++ scala/duckTyping.scala
@@ -0,0 +1,27 @@
+object duckTypes {
+  type DuckLike = { def quack: String; def feathers: String }
+}
+
+class Duck {
+  def quack = "Quaaaaaack!"
+  def feathers = "The duck has white and gray feathers."
+}
+
+class Person {
+  def quack = "The person imitates a duck"
+  def feathers = "The person takes a feather from the ground and shows it."
+  def name = "John Smith"
+}
+
+import duckTypes._
+import scala.languageFeature.reflectiveCalls
+
+def inTheForest(duck: DuckLike) = {
+  println(duck.quack)
+  println(duck.feathers)
+}
+
+val donald = new Duck
+val john = new Person
+inTheForest(donald)
+inTheForest(duck)
